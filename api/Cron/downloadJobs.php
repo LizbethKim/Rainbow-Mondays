f9c10358af8cf17439320bb9dfd5aa1d4c12220a
Michael Pearson <zl2mjp@gmail.com>
2015-08-06 16:55:19 +1200

    Get ready for using cron

--- api/Cron/downloadJobs.php
+++ api/Cron/downloadJobs.php
@@ -1,5 +1,5 @@
 <?php
-include('config.php');
+include('../config.php');
 
 $api = new TrademeJobsApi();
 $api->setConsumerKey('6CEAB3585FFA4AEDB00EF2CFCEFABEF3');
@@ -9,16 +9,16 @@ $api->runQuery();
 
 $daoJobs = new DAO('jobs');
 $batchId = (int)$daoJobs->query("select max(batchid) as batchId from jobs")[0]['batchId'] + 1;
-
 $daoBatch = new DAO('batches');
-$daoBatch->insert(array(
-    'batchId' => $batchId,
-    'date' => time()
-));
 
 foreach($api as $listingId=>$job) {
     $dataset = $job->getDataset();
     $dataset['batchId'] = (int)$batchId;
     $daoJobs->insert($dataset);
-    echo("Listing id: $listingId, title: " . $job->getTitle());
+    echo("Listing id: $listingId, title: " . $job->getTitle() . "\n");
 }
+
+$daoBatch->insert(array(
+    'batchId' => $batchId,
+    'date' => time()
+));
