d91223b00def20f2f100f0eb9b5d9fb75b7ac07e
Michael Pearson <zl2mjp@gmail.com>
2015-08-06 14:49:36 +1200

    End to end filtering.

--- api/download_jobs.php
+++ api/download_jobs.php
@@ -4,7 +4,9 @@ include('config.php');
 $api = new TrademeJobsApi();
 $api->setConsumerKey('xxxxxxxxxxxxxxxxxxxxxxxxxx');
 $api->setSignature('xxxxxxxxxxxxxxxxxxxxxxxxxx');
+$api->updateJobCategories();
 $api->runQuery();
+
 $daoJobs = new DAO('jobs');
 $batchId = (int)$daoJobs->query("select max(batchid) as batchId from jobs")[0]['batchId'] + 1;
 
@@ -12,5 +14,8 @@ foreach($api as $listingId=>$job) {
     $dataset = $job->getDataset();
     $dataset['batchId'] = (int)$batchId;
     $daoJobs->insert($dataset);
-    echo("Listing id: $listingId, title: " . $job->getTitle() . ", Location ID: " . $job->getLocationId() . "\n");
+    echo("Listing id: $listingId, title: " . $job->getTitle()
+        . ", Location ID: " . $job->getLocationId()
+        . ", Category: " . $job->getCategory()
+        . "\n");
 }
