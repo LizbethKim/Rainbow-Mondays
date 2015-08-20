b2f21b227de3f5d3e4be7f5bcf77b7a4357f69cf
Michael Pearson <zl2mjp@gmail.com>
2015-08-20 16:25:22 +1200

    Add the job maker

--- api/Controller.php
+++ api/Controller.php
@@ -19,8 +19,33 @@ class Controller {
         return($build);
     }
 
-    public function getFeedAction() {
+    public function makeJobsAction(){
+        $daoFakeJobs = new DAO('jobs');
+        $fakeJobs = array ();
+        $maxId = $daoFakeJobs->query("SELECT MAX(id) as maxId from districts")[0]["maxId"];
+        $minId = $daoFakeJobs->query("SELECT MIN(id) as minId from districts")[0]["minId"];
+        $batchId = $daoFakeJobs->query("SELECT MAX(batchId) as maxBatchId from jobs")[0]["maxBatchId"];
+        $fakeJobId = $daoFakeJobs->query('SELECT max(id) as maxJobId from jobs')[0]['maxJobId'];
+        //increment batch id somehow ?
 
+        for($a = 0; $a < 15;$a++) {
+            $fakeJobs[] = array(
+                'id' => ++$fakeJobId,
+                'batchId' => $batchId,
+                'locationId' => rand($minId, $maxId),
+                'categoryId' => -1,
+                'listedTime' => time() + rand(10, 30) - (24*60*60)
+            );
+        }
+        foreach($fakeJobs as $fakeJob) {
+            $daoFakeJobs->insert($fakeJob);
+        }
+        return(array(
+            'error' => false
+        ));
+    }
+
+    public function getFeedAction() {
         $jobs = new Jobs();
         return $jobs->getFeed(5*60);
 
