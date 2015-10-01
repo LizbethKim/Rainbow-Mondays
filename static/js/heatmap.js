0dfdef9e98a7444a8cbb897ffaa8845801e8c847
Michael Pearson <zl2mjp@gmail.com>
2015-10-01 13:00:06 +1300

    Change the hover stats to be more like a tooltip

--- static/js/heatmap.js
+++ static/js/heatmap.js
@@ -1,7 +1,4 @@
 $(function () {
-
-
-
     var map,
     zoom = 6,
     heatmap,
@@ -16,24 +13,7 @@ $(function () {
         disableDefaultUI: true,
         panControl: true,
         maxZoom: 11,
-        minZoom: zoom//,
-        /*styles: [{
-            "stylers": [
-                {
-                    "visibility": "off"
-                }
-            ]
-        }, {
-            "featureType": "administrative",
-            "stylers": [{
-                "visibility": "simplified"
-            }]
-        }, {
-            "featureType": "water",
-            "stylers": [{
-                "visibility": "simplified"
-            }]
-        }]*/
+        minZoom: zoom,
     }, updateData = function (rawData) {
 
 
@@ -64,39 +44,28 @@ $(function () {
     };
 
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-    console.log(map);
-
-
-      //var lastValidCenter = map.getCenter();
-      //console.log(map.getCenter());
-      //	      var allowedBounds = map.getBounds();
-      //console.log(allowedBounds.getBounds().getNorthEast());
-
-
-
 
   $.ajax({
         url: '/api/list',
         success: updateData
     });
 
-
     var currentRegion;
 
     var getCenter = function(x, y) {
-      var subCat = $('#subcategory-selection').val();
-      var cat = $('#category-selection').val();
-      if(subCat == 0) {
-          subCat = cat;
-      }
-      return {
-        category: subCat,
-        time: $('#timeslider-input').val(),
-        lat: x,
-        lng: y,
-        level: 0
-      }
-    }
+        var subCat = $('#subcategory-selection').val();
+        var cat = $('#category-selection').val();
+        if (subCat == 0) {
+            subCat = cat;
+        }
+        return {
+            category: subCat,
+            time: $('#timeslider-input').val(),
+            lat: x,
+            lng: y,
+            level: 0
+        }
+    };
 
     var getOverallCenter = function() {
       console.log(map.getCenter());
@@ -112,33 +81,39 @@ $(function () {
         lat: center.lat(),
         lng: center.lng(),
         level: 0
-      }
-      // return "meow";
+      };
     };
 
-    map.addListener('mousemove', function(event){
-      posX = event.latLng.lat();
-      posY = event.latLng.lng();
-      $.ajax({
-          url: '/api/getInfo',
-          data: getCenter(posX, posY),
-          method: "post",
-          success: function(resp){
-            $(".info").html("Current Region: " + resp[3] + "<br>Number of Jobs: "
-            + (parseInt(resp[0])
-            + parseInt(resp[1])
-            + parseInt(resp[2]))
-            + "<br>Number of FullTime: " + resp[1]
-            + "<br>Number of PartTime: " + resp[0]
-            + "<br>Number of Contract Jobs: " + resp[2]
-            + "<br>Average Age of Listing: "
-            + ((Date.now()/1000 - parseInt(resp[4]['avg(listedTime)']))/(60 * 60 * 24)).toFixed(2) + " days");
-          }
-      });
+    var mouseMoveTimer = 0;
+    var updateStats = function(event){
+        posX = event.latLng.lat();
+        posY = event.latLng.lng();
+        $.ajax({
+            url: '/api/getInfo',
+            data: getCenter(posX, posY),
+            method: "post",
+            success: function(resp){
+                $(".info").html("Current Region: " + resp[3] + "<br>Number of Jobs: "
+                + (parseInt(resp[0])
+                + parseInt(resp[1])
+                + parseInt(resp[2]))
+                + "<br>Number of FullTime: " + resp[1]
+                + "<br>Number of PartTime: " + resp[0]
+                + "<br>Number of Contract Jobs: " + resp[2]
+                + "<br>Average Age of Listing: "
+                + ((Date.now()/1000 - parseInt(resp[4]['avg(listedTime)']))/(60 * 60 * 24)).toFixed(2) + " days");
+                $(".info").css({left:event.pixel.x, top:event.pixel.y}).show();
+            }
+        });
+
+    };
+    map.addListener('mousemove', function () {
+        clearTimeout(mouseMoveTimer);
+        mouseMoveTimer = setTimeout(updateStats.bind(this, arguments[0]), 500);
     });
 
-    $(document).mousemove(function(e){
-      $(".info").css({left:e.pageX, top:e.pageY});
+    $(document).mousemove(function(event){
+        $(".info").hide();
     });
 
     $.ajax({
