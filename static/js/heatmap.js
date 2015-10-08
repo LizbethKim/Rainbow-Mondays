1c7b75566c832eea5b7b1f585c5a3d5f7871c6b4
Shamister <revauthore969@gmail.com>
2015-10-08 15:46:05 +1400

    changed to the map view

--- static/js/heatmap.js
+++ static/js/heatmap.js
@@ -40,8 +40,98 @@ $(function () {
             var b = map.getBounds();
             return b;
     };
+	
+	  var customMapType = new google.maps.StyledMapType(
+	[
+		{
+			"featureType": "administrative",
+			"elementType": "labels.text.fill",
+			"stylers": [
+				{
+					"color": "#444444"
+				}
+			]
+		},
+		{
+			"featureType": "landscape",
+			"elementType": "all",
+			"stylers": [
+				{
+					"color": "#f2f2f2"
+				}
+			]
+		},
+		{
+			"featureType": "poi",
+			"elementType": "all",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "road",
+			"elementType": "all",
+			"stylers": [
+				{
+					"saturation": -100
+				},
+				{
+					"lightness": 45
+				}
+			]
+		},
+		{
+			"featureType": "road.highway",
+			"elementType": "all",
+			"stylers": [
+				{
+					"visibility": "simplified"
+				}
+			]
+		},
+		{
+			"featureType": "road.arterial",
+			"elementType": "labels.icon",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "transit",
+			"elementType": "all",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "water",
+			"elementType": "all",
+			"stylers": [
+				{
+					"color": "#46bcec"
+				},
+				{
+					"visibility": "on"
+				}
+			]
+		}
+	],
+	{
+      name: 'Custom Style'
+	});
+	
+	var customMapTypeId = 'custom_style';
 
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	
+	map.mapTypes.set(customMapTypeId, customMapType);
+	map.setMapTypeId(customMapTypeId);
 
     $(window).bind('mousewheel', function(event) {
         if (event.originalEvent.wheelDelta >= 0) {
